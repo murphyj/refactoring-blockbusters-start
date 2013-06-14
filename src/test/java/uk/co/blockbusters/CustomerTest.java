2f3ab29c74345b9a8eb24b26307996ea2440cea2
James Murphy <jamurphy84@hotmail.com>
2013-06-14 13:47:15 +0100

    Added new Customer Test file (missing from last commit)

---
+++ src/test/java/uk/co/blockbusters/CustomerTest.java
@@ -0,0 +1,13 @@
+package uk.co.blockbusters;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.fail;
+
+public class CustomerTest {
+
+    @Test
+    public void checkStatementForRegularMovieForOneDay() {
+        fail("This test needs to be implemented");
+    }
+}
